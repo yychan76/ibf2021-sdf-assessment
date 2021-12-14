85ad9e4c29ef0883e4c300c01398cd542b642259
Chan Yew Yen <chan.yewyen@gmail.com>
2021-12-14 14:59:52 +0800

    Support png content type header

--- webserver/src/main/java/sdf/HttpClientConnection.java
+++ webserver/src/main/java/sdf/HttpClientConnection.java
@@ -9,6 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
 
 public class HttpClientConnection implements Runnable {
     private Socket socket;
@@ -18,6 +19,7 @@ public class HttpClientConnection implements Runnable {
     private final String OK_RESPONSE_HEADER = "HTTP/1.1 200 OK";
     private final String METHOD_NOT_ALLOWED_HEADER = "HTTP/1.1 405 Method Not Allowed";
     private final String RESOURCE_NOT_FOUND_HEADER = "HTTP/1.1 404 Not Found";
+    private final String PNG_CONTENT_TYPE_HEADER = "Content-Type: image/png";
     private final String DEFAULT_PAGE_RESOURCE = "/index.html";
 
     public HttpClientConnection(Socket socket, List<Path> docPaths) {
@@ -51,7 +53,6 @@ public class HttpClientConnection implements Runnable {
                             break;
                         }
                     } else {
-                        // TODO send method not allowed response
                         sendMethodNotAllowedResponse(method);
                     }
                 }
@@ -82,10 +83,29 @@ public class HttpClientConnection implements Runnable {
         sendResourceNotFoundResponse(resource);
     }
 
+    private Optional<String> getFileExtension(String filename) {
+        return Optional.ofNullable(filename)
+            .filter(f -> f.contains("."))
+            .map(f -> f.substring(f.indexOf(".") + 1));
+    }
+
     private void sendOkResourceResponse(File resourceFile) {
+        Optional<String> opt = getFileExtension(resourceFile.getName().toString());
+        boolean isPng = false;
+        if (opt.isPresent()) {
+            isPng = "png".equalsIgnoreCase(opt.get());
+        }
+        sendOkResourceResponse(resourceFile, isPng);
+    }
+
+    private void sendOkResourceResponse(File resourceFile, boolean isPng) {
         try {
+            System.out.printf("Sending %sresource %s to client%n", isPng ? "png image " : "", resourceFile.getAbsolutePath());
             this.writer.writeString(OK_RESPONSE_HEADER);
             this.writer.writeString(); // empty line
+            if (isPng) {
+                this.writer.writeString(PNG_CONTENT_TYPE_HEADER);
+            }
             byte[] content = Files.readAllBytes(resourceFile.toPath());
             this.writer.writeBytes(content);
         } catch (Exception e) {
